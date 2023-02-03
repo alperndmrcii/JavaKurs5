bce1ee94cc4a121f2b724b5f52ebfd18f8f47cd0
Alperen Demirci <alperndmrcii@gmail.com>
2023-02-03 23:10:29 +0300

    Variables

---
+++ src/Gun02/_06_Degiskenler.java
@@ -0,0 +1,13 @@
+package Gun02;
+
+public class _06_Degiskenler {
+    public static void main(String[] args) {
+    // programda bir veri saklayacaksam (sayi gibi sayac gibi toplam gibi)
+        // benim bilgisayara ne kadar yer istediğimi bir de değişkenin adını
+        // söylemen lazım. int sayi ---> integer yani tam sayı
+
+        int sayi; // hafızada (RAM, geçici hafızada) int kadar bir yer ayırıyor
+        // bu ayrılan yerin adına sayi deniyor. Tip olarak da tam sayi
+        sayi=5;
+    }
+}
