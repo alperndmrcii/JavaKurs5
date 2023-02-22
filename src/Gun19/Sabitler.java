0810651d09172605143854f6703800f946971a3c
Alperen Demirci <alperndmrcii@gmail.com>
2023-02-22 18:50:18 +0300

    Final

---
+++ src/Gun19/Sabitler.java
@@ -0,0 +1,11 @@
+package Gun19;
+
+public class Sabitler {
+
+    final static int birGundekiSaatSayisi=24;
+    final static int birSaattekiDakikaSayisi=60;
+    final static int birDakikadakiSaniyeSayisi=60;
+
+
+
+}
